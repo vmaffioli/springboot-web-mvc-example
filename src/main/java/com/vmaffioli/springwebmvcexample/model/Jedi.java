7da80567c2cd038d42bfc4cf0286890ba3c6b0d6
vmaffioli <vinimaffioli@gmail.com>
2021-09-25 18:34:06 -0300

    add validation

--- src/main/java/com/vmaffioli/springwebmvcexample/model/Jedi.java
+++ src/main/java/com/vmaffioli/springwebmvcexample/model/Jedi.java
@@ -1,8 +1,12 @@
 package com.vmaffioli.springwebmvcexample.model;
 
+import javax.validation.constraints.NotBlank;
+
 public class Jedi {
 
+    @NotBlank
     private String name;
+    @NotBlank
     private String lastName;
 
     public Jedi (final String name, final String lastName) {
